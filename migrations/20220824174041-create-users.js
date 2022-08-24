4ff7a3059aa12310aa0ce6b4be2d765d268f0abb
Your Name <you@example.com>
2022-08-24 15:19:11 -0300

    Conectando com o banco de dados

---
+++ migrations/20220824174041-create-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const UsersTable = queryInterface.createTable("das_usuarios", {
+
+      Nome:{
+        type: Sequelize.STRING,
+        allowNull: false        
+      },
+      CPF:{
+
+        type: Sequelize.BIGINT,
+        allowNull: false,
+        primaryKey: true
+      },
+      Email:{
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      Telefone:{
+        type: Sequelize.BIGINT,
+        allowNull: false
+      },
+      Senha:{
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+
+
+
+    })
+  },
+
+
+
+
+  
+
+  async down (queryInterface) { queryInterface.dropTable("dados_usuarios")
+   
+  }
+}
